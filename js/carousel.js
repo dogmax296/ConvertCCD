4bd534b36786769eb46652bafc68a25cfaff36d5
dogmax296 <ivanov1maxim@gmail.com>
2015-10-24 20:34:10 +0300

    Create all carousels

---
+++ js/carousel.js
@@ -0,0 +1,47 @@
+//CAROUSEL
+
+$(document).ready(function() {
+ 
+  $("#carousel__img").owlCarousel({
+ 
+      autoPlay: 5000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      pagination: false,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
+
+//CAROUSEL TESTIMONIALs
+
+$(document).ready(function() {
+ 
+  $("#carousel__testimonials").owlCarousel({
+ 
+      autoPlay: 7000, //Set AutoPlay to 3 seconds
+      singleItem: true,
+      autoHeight: true
+      
+  });
+ 
+});
+
+//CAROUSEL CLIENTS
+
+$(document).ready(function() {
+ 
+  $("#carousel__clients").owlCarousel({
+ 
+      autoPlay: 5000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      pagination: false,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
